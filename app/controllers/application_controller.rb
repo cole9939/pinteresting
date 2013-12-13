97d657915de6fbac0cf4a55c03b3e42a39903f37
Rohit <rohitp.ror@gmail.com>
2013-12-13 22:21:28 +0530

    Worked into signup form, add website field and change name as a username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,8 @@ protected
  def configure_permitted_parameters
    devise_parameter_sanitizer.for(:sign_up) << :name
    devise_parameter_sanitizer.for(:account_update) << :name
+   devise_parameter_sanitizer.for(:sign_up) << :url
+   devise_parameter_sanitizer.for(:account_update) << :url
  end
+ 
 end
